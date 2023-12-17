698c4d083208e07b6c0dfa9a3db66e83bffd32ca
Mansur Hamidov <mansur.hamidov.19@gmail.com>
2023-12-18 01:22:37 +0200

    small refactoring

--- src/components/Dropdown/DropdownMenu.tsx
+++ src/components/Dropdown/DropdownMenu.tsx
@@ -9,7 +9,7 @@ function DropdownMenu(props: ParentProps<{class?: string}>) {
   return (
     <div
       class={classNames(props.class, {
-        "transition-[opacity,margin] duration hs-dropdown-open:opacity-100 min-w-[6rem] bg-white shadow-md rounded-lg p-2 mt-2 divide-y divide-gray-200 dark:bg-gray-800 dark:border dark:border-gray-700 dark:divide-gray-700": true,
+        "transition-[opacity,margin] duration hs-dropdown-open:opacity-100 min-w-[17ch] bg-white shadow-md rounded-lg p-2 mt-2 divide-y divide-gray-200 dark:bg-gray-800 dark:border dark:border-gray-700 dark:divide-gray-700": true,
         "block absolute m-0 opacity-1 translate-y-10": dropdownContext.isOpen(),
         "opacity-0 hidden": !dropdownContext.isOpen(),
         "left-auto right-0": dropdownContext.horizontalAlign === "right",
--- src/components/TextInputWithFloatingLabel/index.tsx
+++ src/components/TextInputWithFloatingLabel/index.tsx
@@ -41,7 +41,7 @@ export function TextInputWithFloatingLabel(props: TextInputWithFloatingLabelProp
             "block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm font-medium bg-secondary-50 dark:bg-secondary-700 border-0 border-b-2 appearance-none dark:text-white focus:outline-none focus:ring-0 placeholder-transparent focus:placeholder-secondary-400 peer": true,
             "ps-10": Boolean(localProps.addonStart),
             "border-secondary-200 dark:border-secondary-600 dark:focus:border-primary-500 focus:border-primary-600": !hasError(),
-            "border-red-600 dark:border-red-500": hasError()
+            "border-red-600 dark:border-red-400": hasError()
           }}
           {...nativeProps}
         />
@@ -59,10 +59,10 @@ export function TextInputWithFloatingLabel(props: TextInputWithFloatingLabelProp
         <label
           for={nativeProps.id}
           classList={{
-            "absolute text-sm font-medium text-secondary-400 dark:text-secondary-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto": true,
+            "absolute text-sm font-medium text-secondary-500 dark:text-secondary-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto": true,
             "ps-10 peer-focus:ps-10": Boolean(localProps.addonStart),
             "peer-focus:text-primary-700 peer-focus:dark:text-primary-500": !hasError(),
-            "peer-focus:text-red-700 peer-focus:dark:text-red-500": hasError(),
+            "peer-focus:text-red-700 peer-focus:dark:text-red-400": hasError(),
           }}
         >
           {props.label}
