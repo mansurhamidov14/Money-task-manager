357d675395b53db06b8acd32162083b69866cc80
Mansur Hamidov <mansur.hamidov.19@gmail.com>
2023-12-18 04:45:17 +0200

    added legacy build plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
-import path from 'path'
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import path from 'path';
+import { defineConfig } from 'vite';
+import solid from 'vite-plugin-solid';
+import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
-  plugins: [solid()],
+  base: './',
+  build: {
+    target: 'es2015'
+  },
+  plugins: [
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    }),
+    solid()
+  ],
   resolve: {
     alias: [
       { find: '@app', replacement: path.resolve(__dirname, 'src') },
