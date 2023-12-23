5ba26333087ea7465c383783a5aaaebef257fe92
Mansur Hamidov <mansur.hamidov.19@gmail.com>
2023-12-23 22:40:51 +0200

    skin names

--- src/screens/NewAccountScreen/FormFields/SkinSelect.tsx
+++ src/screens/NewAccountScreen/FormFields/SkinSelect.tsx
@@ -1,7 +1,9 @@
 import { Field } from "solid-form-handler";
 import { InputProps } from "./types";
-import { For } from "solid-js";
+import { For, Show } from "solid-js";
 import { skins } from "@app/constants";
+import { Message } from "@app/i18n";
+import { AiFillCheckCircle } from "solid-icons/ai";
 
 export function SkinSelect(props: InputProps) {
   return (
@@ -15,7 +17,7 @@ export function SkinSelect(props: InputProps) {
             <div class="grid grid-rows-3 grid-flow-col w-fit gap-3" style="grid-template-rows: auto auto;">
               <For each={skins}>
                 {(skin, index) => (
-                  <div class="w-[8rem]">
+                  <div class="w-[8rem] relative">
                     <input
                       hidden
                       {...field.props}
@@ -28,15 +30,25 @@ export function SkinSelect(props: InputProps) {
                       value={index()}
                       type="radio"
                     />
-                    <label for={`${field.props.id}-${index()}`}>
-                      <img
-                        classList={{
-                          "w-full opacity-100 rounded-md": true,
-                          "opacity-50": !field.helpers.isChecked(index())
-                        }}
-                        src={skin.image}
-                      />
+                    <label
+                      classList={{
+                        "relative opacity-100": true,
+                        "opacity-50": !field.helpers.isChecked(index())
+                      }}
+                      for={`${field.props.id}-${index()}`}
+                    >
+                      <img class="w-full rounded-md" src={skin.image} />
+                      <span class="text-xs">
+                        <Message>{`NewAccountScreen.FormFields.skins.${index()}`}</Message>
+                      </span>
                     </label>
+                    <Show when={field.helpers.isChecked(index())}>
+                      <div class="text-green-500 absolute top-0 left-0 w-full h-full flex justify-center items-center text-4xl pb-7">
+                        <div class="bg-white rounded-full">
+                          <AiFillCheckCircle />
+                        </div>
+                      </div>
+                    </Show>
                   </div>
                 )}
               </For>
