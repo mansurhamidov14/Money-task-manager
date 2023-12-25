386117aa7e7bf2c23a210f32cca7325c7ecbb546
Mansur Hamidov <mansur.hamidov.19@gmail.com>
2023-12-25 05:36:19 +0200

    screen header update

--- src/components/ScreenHeader/index.tsx
+++ src/components/ScreenHeader/index.tsx
@@ -1,6 +1,6 @@
 import { JSX, Show } from "solid-js";
+import { FaSolidChevronLeft } from "solid-icons/fa";
 import { Button } from "../Button";
-import { IoCloseOutline } from "solid-icons/io";
 
 export type ScreenHeaderProps = {
   withGoBackButton?: boolean;
@@ -12,8 +12,8 @@ export function ScreenHeader(props: ScreenHeaderProps) {
   return (
     <div class="flex gap-3 h-14 content-center relative items-center border-b bg-white border-secondary-200/30 dark:bg-gray-900/40 px-2 shadow dark:border-gray-700">
       <Show when={props.withGoBackButton}>
-        <Button class="absolute rounded-full" variant="transparent" size="md" square onClick={() => history.back()}>
-          <IoCloseOutline class="text-2xl" />
+        <Button class="absolute" variant="transparent" size="md" square onClick={() => history.back()}>
+          <FaSolidChevronLeft size={24} />
         </Button>
       </Show>
       <div class="text-xl font-bold flex-1 flex justify-center items-center">
