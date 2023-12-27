09a908ce8c28488c0a5cfed9586651ed8525e063
Mansur Hamidov <mansur.hamidov.19@gmail.com>
2023-12-27 02:14:41 +0200

    fixed bug

--- src/screens/AuthScreen/LoginPage/index.tsx
+++ src/screens/AuthScreen/LoginPage/index.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "@solidjs/router";
+import { Navigate, useNavigate } from "@solidjs/router";
 import { Field, useFormHandler } from "solid-form-handler";
 import { yupSchema } from "solid-form-handler/yup";
 import { IoKey } from "solid-icons/io";
@@ -11,6 +11,7 @@ import { userService } from "@app/services";
 import { Link, accountsStore, transactionsStore, toastStore, user } from "@app/stores";
 
 import { AuthLayout } from "../AuthLayout";
+import { Show } from "solid-js";
 
 export function LoginPage() {
   const formHandler = useFormHandler(yupSchema(getLoginFormSchema()), {
@@ -33,55 +34,57 @@ export function LoginPage() {
   }
 
   return (
-    <AuthLayout>
-      <form onSubmit={handleSubmit} class="flex flex-col gap-5 justify-around px-5">
-        <Field
-          mode="input"
-          name="email"
-          formHandler={formHandler}
-          render={(field) => (
-            <TextInput
-              autocomplete="off"
-              id="email"
-              label={t("AuthScreen.FormFields.Email.label")}
-              placeholder="youremail@example.com"
-              addonStart={<FiAtSign />}
-              errorMessage={field.helpers.errorMessage}
-              {...field.props}
-            />
-          )}
-        />
-        <Field
-          mode="input"
-          name="password"
-          formHandler={formHandler}
-          render={(field) => (
-            <TextInput
-              id="password"
-              type="password"
-              label={t("AuthScreen.FormFields.Password.label")}
-              placeholder="••••••••••"
-              addonStart={<IoKey />}
-              errorMessage={field.helpers.errorMessage}
-              {...field.props}
-            />
-          )}
-        />
-        <Button variant="primary" size="lg" type="submit">
-          <Action>SignIn</Action>
-        </Button>
-        <div class="text-sm pt-10 pb-4 font-medium">
-          <span class="flex gap-2 justify-center">
-            <Message>AuthScreen.dontHaveAnAccount</Message>
-            {" "}
-            <Link class="text-primary-500 dark:text-primary-400 font-semibold" href="/auth/signup">
-              <Message>
-                AuthScreen.signUp
-              </Message>
-            </Link>
-          </span>
-        </div>
-      </form>
-    </AuthLayout>
+    <Show when={user.currentUser().status === "unauthorized"} fallback={<Navigate href="/auth/pin" />}>
+      <AuthLayout>
+        <form onSubmit={handleSubmit} class="flex flex-col gap-5 justify-around px-5">
+          <Field
+            mode="input"
+            name="email"
+            formHandler={formHandler}
+            render={(field) => (
+              <TextInput
+                autocomplete="off"
+                id="email"
+                label={t("AuthScreen.FormFields.Email.label")}
+                placeholder="youremail@example.com"
+                addonStart={<FiAtSign />}
+                errorMessage={field.helpers.errorMessage}
+                {...field.props}
+              />
+            )}
+          />
+          <Field
+            mode="input"
+            name="password"
+            formHandler={formHandler}
+            render={(field) => (
+              <TextInput
+                id="password"
+                type="password"
+                label={t("AuthScreen.FormFields.Password.label")}
+                placeholder="••••••••••"
+                addonStart={<IoKey />}
+                errorMessage={field.helpers.errorMessage}
+                {...field.props}
+              />
+            )}
+          />
+          <Button variant="primary" size="lg" type="submit">
+            <Action>SignIn</Action>
+          </Button>
+          <div class="text-sm pt-10 pb-4 font-medium">
+            <span class="flex gap-2 justify-center">
+              <Message>AuthScreen.dontHaveAnAccount</Message>
+              {" "}
+              <Link class="text-primary-500 dark:text-primary-400 font-semibold" href="/auth/signup">
+                <Message>
+                  AuthScreen.signUp
+                </Message>
+              </Link>
+            </span>
+          </div>
+        </form>
+      </AuthLayout>
+    </Show>
   );
 }
--- src/screens/AuthScreen/SignUpPage/index.tsx
+++ src/screens/AuthScreen/SignUpPage/index.tsx
@@ -1,5 +1,5 @@
-import { useNavigate } from "@solidjs/router";
-import { For } from "solid-js";
+import { Navigate, useNavigate } from "@solidjs/router";
+import { For, Show } from "solid-js";
 import { yupSchema } from "solid-form-handler/yup";
 import { Field, useFormHandler } from "solid-form-handler";
 import { IoKey } from "solid-icons/io";
@@ -58,107 +58,109 @@ export function SignUpPage() {
   }
 
   return (
-    <AuthLayout>
-      <form onSubmit={handleSubmit} class="flex flex-col gap-5 justify-around px-5">
-        <Field
-          mode="input"
-          name="firstName"
-          formHandler={formHandler}
-          render={(field) => (
-            <TextInput
-              autocomplete="off"
-              id="firstName"
-              label={t("AuthScreen.FormFields.FirstName.label")}
-              placeholder={t("AuthScreen.FormFields.FirstName.placeholder")}
-              errorMessage={field.helpers.errorMessage}
-              {...field.props}
-            />
-          )}
-        />
-        <Field
-          mode="input"
-          name="lastName"
-          formHandler={formHandler}
-          render={(field) => (
-            <TextInput
-              autocomplete="off"
-              id="lastName"
-              label={t("AuthScreen.FormFields.LastName.label")}
-              placeholder={t("AuthScreen.FormFields.LastName.placeholder")}
-              errorMessage={field.helpers.errorMessage}
-              {...field.props}
-            />
-          )}
-        />
-        <Field
-          mode="input"
-          name="email"
-          formHandler={formHandler}
-          render={(field) => (
-            <TextInput
-              autocomplete="off"
-              id="email"
-              label={t("AuthScreen.FormFields.Email.label")}
-              placeholder="youremail@example.com"
-              addonStart={<FiAtSign class="text-lg" />}
-              errorMessage={field.helpers.errorMessage}
-              {...field.props}
-            />
-          )}
-        />
-        <Field
-          mode="input"
-          name="newPassword"
-          formHandler={formHandler}
-          render={(field) => (
-            <TextInput
-              id="newPassword"
-              type="password"
-              label={t("AuthScreen.FormFields.Password.label")}
-              placeholder="••••••••••"
-              addonStart={<IoKey class="text-lg" />}
-              errorMessage={field.helpers.errorMessage}
-              {...field.props}
-            />
-          )}
-        />
-        <Field
-          mode="input"
-          name="primaryCurrency"
-          formHandler={formHandler}
-          render={(field) => (
-            <Select
-              id="primaryCurrency"
-              label={t("AuthScreen.FormFields.Currency.label")}
-              addonStart={<img src={currencies[formHandler.getFieldValue("primaryCurrency") as CurrencyCode]?.flag} class="w-full" />}
-              errorMessage={field.helpers.errorMessage}
-              {...field.props}
-            >
-              <For each={Object.values(currencies)}>
-                {currency => (
-                  <option value={currency.code}>
-                    {t(`AuthScreen.FormFields.Currency.Options.${currency.code}`)}
-                  </option>
-                )}
-              </For>
-            </Select>
-          )}
-        />
-        <Button variant="primary" size="lg" type="submit">
-          <Action>SignUp</Action>
-        </Button>
-        <div class="text-sm pt-3 pb-4 font-medium">
-          <span class="flex justify-center gap-2">
-            <Message>AuthScreen.alreadyHaveAnAccount</Message>
-            {" "}
-            <Link class="text-primary-500 dark:text-primary-400 font-semibold" href="/auth">
-              <Message>
-                AuthScreen.signIn
-              </Message>
-            </Link>
-          </span>
-        </div>
-      </form>
-    </AuthLayout>
+    <Show when={user.currentUser().status === "unauthorized"} fallback={<Navigate href="/auth/pin" />}>
+      <AuthLayout>
+        <form onSubmit={handleSubmit} class="flex flex-col gap-5 justify-around px-5">
+          <Field
+            mode="input"
+            name="firstName"
+            formHandler={formHandler}
+            render={(field) => (
+              <TextInput
+                autocomplete="off"
+                id="firstName"
+                label={t("AuthScreen.FormFields.FirstName.label")}
+                placeholder={t("AuthScreen.FormFields.FirstName.placeholder")}
+                errorMessage={field.helpers.errorMessage}
+                {...field.props}
+              />
+            )}
+          />
+          <Field
+            mode="input"
+            name="lastName"
+            formHandler={formHandler}
+            render={(field) => (
+              <TextInput
+                autocomplete="off"
+                id="lastName"
+                label={t("AuthScreen.FormFields.LastName.label")}
+                placeholder={t("AuthScreen.FormFields.LastName.placeholder")}
+                errorMessage={field.helpers.errorMessage}
+                {...field.props}
+              />
+            )}
+          />
+          <Field
+            mode="input"
+            name="email"
+            formHandler={formHandler}
+            render={(field) => (
+              <TextInput
+                autocomplete="off"
+                id="email"
+                label={t("AuthScreen.FormFields.Email.label")}
+                placeholder="youremail@example.com"
+                addonStart={<FiAtSign class="text-lg" />}
+                errorMessage={field.helpers.errorMessage}
+                {...field.props}
+              />
+            )}
+          />
+          <Field
+            mode="input"
+            name="newPassword"
+            formHandler={formHandler}
+            render={(field) => (
+              <TextInput
+                id="newPassword"
+                type="password"
+                label={t("AuthScreen.FormFields.Password.label")}
+                placeholder="••••••••••"
+                addonStart={<IoKey class="text-lg" />}
+                errorMessage={field.helpers.errorMessage}
+                {...field.props}
+              />
+            )}
+          />
+          <Field
+            mode="input"
+            name="primaryCurrency"
+            formHandler={formHandler}
+            render={(field) => (
+              <Select
+                id="primaryCurrency"
+                label={t("AuthScreen.FormFields.Currency.label")}
+                addonStart={<img src={currencies[formHandler.getFieldValue("primaryCurrency") as CurrencyCode]?.flag} class="w-full" />}
+                errorMessage={field.helpers.errorMessage}
+                {...field.props}
+              >
+                <For each={Object.values(currencies)}>
+                  {currency => (
+                    <option value={currency.code}>
+                      {t(`AuthScreen.FormFields.Currency.Options.${currency.code}`)}
+                    </option>
+                  )}
+                </For>
+              </Select>
+            )}
+          />
+          <Button variant="primary" size="lg" type="submit">
+            <Action>SignUp</Action>
+          </Button>
+          <div class="text-sm pt-3 pb-4 font-medium">
+            <span class="flex justify-center gap-2">
+              <Message>AuthScreen.alreadyHaveAnAccount</Message>
+              {" "}
+              <Link class="text-primary-500 dark:text-primary-400 font-semibold" href="/auth">
+                <Message>
+                  AuthScreen.signIn
+                </Message>
+              </Link>
+            </span>
+          </div>
+        </form>
+      </AuthLayout>
+    </Show>
   );
 }
