938c7b560c095fcafd712a2c741e6378eeb3ce4f
Mansur Hamidov <mansur.hamidov.19@gmail.com>
2024-02-10 19:09:51 +0200

    scripts refactoring

--- rename-android-build.mjs
+++ rename-android-build.mjs
@@ -7,12 +7,15 @@ const defaultBuildName = `${buildPath}/app-debug.apk`;
 try {
   const jsonData = JSON.parse(fs.readFileSync(buildJsonData));
   fs.accessSync(defaultBuildName);
-  const buildFileName = `./releases/wfo-v${jsonData.elements[0].versionName}.apk`;
+  const buildFileName = `./releases/android/wfo-v${jsonData.elements[0].versionName}.apk`;
   try {
     fs.accessSync(buildFileName);
     fs.unlinkSync(buildFileName);
   } catch {} finally {
     fs.renameSync(defaultBuildName, buildFileName);
+    const builtFilePath = fs.realpathSync(buildFileName);
+    console.log("Build completed. Built apk:");
+    console.log("        " + builtFilePath);
   }
 } catch (e) {
   console.error(e);
