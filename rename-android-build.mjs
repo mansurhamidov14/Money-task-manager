504d29d755a10504408ff09b57018b33ebb7745c
Mansur Hamidov <mansur.hamidov.19@gmail.com>
2024-02-10 18:21:20 +0200

    automated commands for version upgrade & renaming build file

---
+++ rename-android-build.mjs
@@ -0,0 +1,20 @@
+import fs from "fs";
+
+const buildPath = "./platforms/android/app/build/outputs/apk/debug";
+const buildJsonData = `${buildPath}/output-metadata.json`
+const defaultBuildName = `${buildPath}/app-debug.apk`;
+
+try {
+  const jsonData = JSON.parse(fs.readFileSync(buildJsonData));
+  fs.accessSync(defaultBuildName);
+  const buildFileName = `./releases/wfo-v${jsonData.elements[0].versionName}.apk`;
+  try {
+    fs.accessSync(buildFileName);
+    fs.unlinkSync(buildFileName);
+  } catch {} finally {
+    fs.renameSync(defaultBuildName, buildFileName);
+  }
+} catch (e) {
+  console.error(e);
+  console.log("There is no apk build found");
+}
